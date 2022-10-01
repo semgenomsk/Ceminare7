0aca774449feff1218c97250dd4af45560782c0d
Semen <semgenomsk@yandex.ru>
2022-10-01 15:26:06 +0600

    Done task 49

---
+++ task49/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, 
+// у которых оба индекса чётные, и замените эти элементы 
+// на их квадраты.
+
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Новый массив будет выглядеть
+// вот так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetMatrix(int rows, int cols, int leftRange = -10, int rightRange = 10)
+{
+    Random rand = new Random();
+    int[,] matrix = new int[rows, cols];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+void GetElementsPow(int[,] matrix)
+{
+    for (int i = 1; i < matrix.GetLength(0); i += 2)
+    {
+        for (int j = 1; j < matrix.GetLength(1); j += 2)
+        {
+            matrix[i, j] = matrix[i, j] * matrix[i, j];
+        }
+    }
+}
+
+int m = ReadNumber("Введите количество строк: ");
+int n = ReadNumber("Введите количество столбцов: ");
+int[,] matr = GetMatrix(m, n);
+PrintMatrix(matr);
+GetElementsPow(matr);
+PrintMatrix(matr);
+
+//Найдите элементы, у которых оба индекса чётные, и замените эти элементы 
+// на их квадраты.
+
+// void GetElementsPow(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (i % 2 == 0 && int j % 2 == 0)
+//             matrix[i,j] = matrix[i,j] * matrix[i,j];
+//         }
+//     }
+// }
