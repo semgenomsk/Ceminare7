49c9a899b4e25b1cd16832cc5677b3d194c91cf8
Semen <semgenomsk@yandex.ru>
2022-10-01 15:26:54 +0600

    Done task 51

---
+++ task51/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, 
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetMatrix(int rows, int cols, int leftRange = -10, int rightRange = 10)
+{
+    Random rand = new Random();
+    int[,] matrix = new int[rows, cols];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumMainDiagonal(int[,] matrix)
+{
+    int sum = 0;
+    int min;
+    if (matrix.GetLength(0) >= matrix.GetLength(1))
+    {
+        min = matrix.GetLength(1);
+    }
+    else
+    {
+        min = matrix.GetLength(0);
+    }
+    for (int i = 0; i < min; i++)
+    {
+        sum = sum + matrix[i, i];
+    }
+    return sum;
+}
+
+int m = ReadNumber("Введите количество строк: ");
+int n = ReadNumber("Введите количество столбцов: ");
+int[,] matr = GetMatrix(m, n);
+PrintMatrix(matr);
+Console.WriteLine($"Сумма элементов находящихся по диагонале: {SumMainDiagonal(matr)}");
